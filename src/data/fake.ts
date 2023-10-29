5ae3ed25b24dd994847ba7f63156b854b06b48e9
Jae Gifford <jae.giff@outlook.com>
2023-10-29 10:26:20 -0700

    adding fake datta set classes etc.

---
+++ src/data/fake.ts
@@ -0,0 +1,76 @@
+import uniqid from 'uniqid';
+import { faker } from '@faker-js/faker';
+
+class DummyUser {
+  id: string;
+  name: string;
+  email: string;
+  constructor(name: string, email: string) {
+    this.id = uniqid();
+    this.name = name;
+    this.email = email;
+  }
+}
+class DummyPantry {
+  id: string;
+  constructor() {
+    this.id = uniqid();
+  }
+}
+class DummyRecipe {
+  id: string;
+  constructor() {
+    this.id = uniqid();
+  }
+}
+class DummyFoodItem {
+  id: string;
+  name: string;
+  quantity: number;
+  expiry: Date;
+  constructor(name: string, quantity: number, expiry: Date) {
+    this.id = uniqid();
+    this.name = name;
+    this.quantity = quantity;
+    this.expiry = expiry;
+  }
+}
+
+export function generateDummyUsers(quantity: number) {
+  let result = [];
+  for (let i = 0; i < quantity; i++) {
+    result.push(
+      new DummyUser(faker.person.firstName(), faker.internet.email())
+    );
+  }
+  return result;
+}
+
+export function generateDummyPantries(quantity: number) {
+  let result = [];
+  for (let i = 0; i < quantity; i++) {
+    result.push(new DummyPantry());
+  }
+  return result;
+}
+
+export function generateDummyRecipe(quantity: number) {
+  let result = [];
+  for (let i = 0; i < quantity; i++) {
+    result.push(new DummyRecipe());
+  }
+  return result;
+}
+export function generateDummyFoodItem(quantity: number) {
+  let result = [];
+  for (let i = 0; i < quantity; i++) {
+    result.push(
+      new DummyFoodItem(
+        faker.commerce.product(),
+        faker.number.int(999),
+        faker.date.soon()
+      )
+    );
+  }
+  return result;
+}
